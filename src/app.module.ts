3d29d2f6a793e27907e43f2132723a76f053c5be
Владислав Бублик <bublik1170@gmail.com>
2022-04-14 13:53:26 +0300

    Add TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({isGlobal: true, envFilePath: './.env'}),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        type: config.get<'aurora-data-api'>('POSTGRES_CONNECTION'),
+        username: config.get<string>('POSTGRES_USER'),
+        password: config.get<string>('POSTGRES_PASSWORD'),
+        database: config.get<string>('POSTGRES_DATABASE'),
+        port: config.get<number>('POSTGRES_PORT'),
+        entities: [__dirname + 'dist/**/*.entity{.ts, .js}'],
+        synchronize: false,
+        autoLoadEntities: true,
+        logging: true
+      })
+    }),
+    UsersModule
+  ],
   providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = await app.get(ConfigService)
+  const port = config.get<number>('PORT')
+  await app.listen(port || 3000, ()=> { 
+    console.log(`App started on port ${port}`)
+  });
 }
 bootstrap();
